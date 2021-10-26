3682604b08cd215fb15de97df66716bf0ec7ae90
zhaoxinghua123321 <674608896@qq.com>
2021-10-26 13:34:54 +0800

    55

--- src/com/zhao/test/Test.java
+++ src/com/zhao/test/Test.java
@@ -2,6 +2,6 @@ package com.zhao.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("helloqq4446688");
+        System.out.println("helloqq11");
     }
 }
