50cf0846e74a1be0c28c70ba3748758fccd7a49b
huage <674608896@qq.com>
2021-10-26 11:11:20 +0800

    第一版

--- src/com/zhao/test/Test.java
+++ src/com/zhao/test/Test.java
@@ -2,6 +2,6 @@ package com.zhao.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("helloqq");
+        System.out.println("helloqq444");
     }
 }
